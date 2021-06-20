3e32f58ae4c0d88db7d44ace07b3c6eb64f6521e
Shubham Dixit <shubhamdixit19950321>
2021-06-20 19:42:28 +0530

    Added object order property basics - a cool feature

--- objects/basics.js
+++ objects/basics.js
@@ -8,3 +8,13 @@ console.log('response',  'name1' in testObject)
 for ( let key in testObject) {
   console.log('key = ', key, ' value = ', testObject[key])
 }
+
+// so objects have a cool property when named objects used with number are ordered well
+const orderedObject = {
+  '34': 'testy',
+  '45': 'woww',
+  '29': 'is that real?',
+  '5': 'lets see'
+}
+
+console.log('Ordered well', orderedObject) //Ordered well { '5': 'lets see', '29': 'is that real?', '34': 'testy', '45': 'woww' }
