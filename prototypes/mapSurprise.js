c1870574eb825279a19ca5372a1f7e412fd33080
Shubham Dixit <shubhamdixit19950321>
2021-06-21 17:12:58 +0530

    Added moinor change

--- prototypes/mapSurprise.js
+++ prototypes/mapSurprise.js
@@ -1,11 +1,9 @@
-//  /*
-  const val = [1, 2, 3, 4, 5]
+const val = [1, 2, 3, 4, 5]
 
-  const result = val.map(function(item, index) {
-    val.push(index)
-    console.log('Value is ', val, val.length)
-    return item * 5
-  } )
+const result = val.map(function(item, index) {
+  val.push(index)
+  console.log('Value is ', val, val.length)
+  return item * 5
+} )
 
-  console.log('result is = ', result) // result is =  [ 5, 10, 15, 20, 25 ]
-// */
+console.log('result is = ', result) // result is =  [ 5, 10, 15, 20, 25 ]
