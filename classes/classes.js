26557d9d35ef403366af6396a488f1ca19821242
Shubham Dixit <shubhamdixit19950321>
2021-06-27 20:05:23 +0530

    Added explanation for classes

---
+++ classes/classes.js
@@ -0,0 +1,39 @@
+function Car(model, color) { 
+  this.model = model
+  this.color = color
+}
+
+const carObject = new Car('Sedan', 'Daytona Grey')
+
+Car.prototype.getCarColor = function() {
+  return this.color
+}
+
+console.log('object Access = ', carObject.color) // Daytona Grey
+Car.call(carObject, 'Hatchback', 'Professional Silver')
+console.log('Car color updated?', carObject.color ) // Professional Silver
+console.log('From Prototype', carObject.getCarColor()) // Professional Silver
+
+
+// Classes are called the Syntactical Sugar for above Implementation because --- See Below:
+class CarClass {
+  constructor(model, color) {
+      this.model = model
+      this.color = color
+  }
+  getCarColor() {
+    return this.color
+  }
+  setCarColor(color) {
+    this.color = color
+  }
+  static getCarColorModel() {
+    return `This os a static method returning similar values`
+  }
+}
+
+const carClassObject = new CarClass('LMV', 'Surprise Orange')
+console.log('Car Class Color ', carClassObject.getCarColor())
+console.log('See whether Class color updated? ', CarClass.getCarColorModel() )
+
+// Looks similar?? But stll classes have various nuances and they are not very similar
