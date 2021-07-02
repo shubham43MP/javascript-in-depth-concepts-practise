cd08e92b953c75621c22fc9601257aab97b037ca
Shubham Dixit <shubhamdixit19950321>
2021-07-02 22:23:39 +0530

    Added better editing

--- AlgoInJS/DSinJS/stackFromPriorityQ.js
+++ AlgoInJS/DSinJS/stackFromPriorityQ.js
@@ -1,9 +1,14 @@
 /*
-  To Implement stack from Priority Queue we will import the classes QueueElement and PriorityQueue from priorityQueue.js
+  To Implement stack from Priority Queue we will import the classes QueueElement and PriorityQueue from
+  priorityQueue.js
 
-  Import export of ES6 doesnt work quite a well with vanilla JS in node hence here we assume that the reader has seen the priortyQueue file and take reference from there. If your code doesnt work, copy paste the whole classes of QueueElement and PriorityQueue. The idea is to demonstrate Proof of Concept for the stackFromPQ rather than solving Nodejs project issues
+  Import export of ES6 doesnt work quite a well with vanilla JS in node hence here we assume that the reader has
+  seen the priortyQueue file and take reference from there. If your code doesnt work, copy paste the whole classes
+  of QueueElement and PriorityQueue. The idea is to demonstrate Proof of Concept for the stackFromPQ rather than
+  solving Nodejs project issues
 
-  Idea Behind this is that we will keep assiging the highest priority to the new added element and in that way we can have the LIFO System
+  Idea Behind this is that we will keep assiging the highest priority to the new added element and in that way we
+  can have the LIFO System
 */
 
 import { QueueElement, PriorityQueue } from './priorityQueue'
