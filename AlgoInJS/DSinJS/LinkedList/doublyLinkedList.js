bf084fd8c93a631353bd70c8686db850e5f4ed1e
Shubham Dixit <shubhamdixit19950321>
2021-07-04 17:10:15 +0530

    Added a basic setup for doubly linked list

--- AlgoInJS/DSinJS/LinkedList/doublyLinkedList.js
+++ AlgoInJS/DSinJS/LinkedList/doublyLinkedList.js
@@ -6,11 +6,11 @@ We will perform all operations written here with our flavour of code:
 https://learnersbucket.com/tutorials/data-structures/doubly-linked-list-implementation-in-javascript/
 */
 
-class Node {
+class DLLNode {
   constructor(element){
-    this.prevElementRef = null,
-    this.element = element,
-    this.nextElementRef = null,
+    this.prevElementRef = null;
+    this.element = element;
+    this.nextElementRef = null;
   }
 }
 
@@ -18,13 +18,47 @@ class DoublyLinkedList {
   constructor() {
     this.head = null, // Head Contains first element
     this.tail = null, // Tail Contains last element
-    this.size = 0
+    this.length = 0
   }
+
+  /*
+  Displays a Doubly Linked list in specified direction
+  */
+ displayDoublyLinkedList() {
+   if(this.length > 0) {
+     let currentNode = this.head
+     let str = ''
+     while(currentNode.nextElementRef !== null) {
+      str += `${ currentNode.element }-->`
+      currentNode = currentNode.nextElementRef;
+     }
+     console.log(str)
+   } else {
+     console.log('Linked list is empty')
+   }
+ }
 
   /*
   Adds an element to the end of the DLL
   */
   append(element) {
-
+    const node = new DLLNode(element)
+    if(this.length === 0) {
+      this.head = node;
+      this.tail = node;
+      console.log(this.head)
+    }
+    else {
+      this.tail.next = node
+      node.prevElementRef = this.tail;
+      this.tail = node
+    }
+    this.length++
   }
 }
+
+const dll = new DoublyLinkedList()
+dll.append(45)
+dll.append(46)
+dll.append(47)
+dll.displayDoublyLinkedList()
