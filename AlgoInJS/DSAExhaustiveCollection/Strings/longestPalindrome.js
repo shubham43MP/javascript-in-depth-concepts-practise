8b4f0f4c535245d442b314a0722c6436ed5fcf06
shubham43MP <shubhamdixit19950321@gmail.com>
2021-08-07 17:37:49 +0530

    [DSA] Added base solution for longest palindrome

---
+++ AlgoInJS/DSAExhaustiveCollection/Strings/longestPalindrome.js
@@ -0,0 +1,52 @@
+/**
+ * Given a string S, find the longest palindromic substring in S.
+ * Substring of string S: S[ i . . . . j ] where 0 ≤ i ≤ j < len(S).
+ * Palindrome string: A string which reads the same backwards. More formally, S is palindrome if reverse(S) = S.
+ * Incase of conflict, return the substring which occurs first ( with the least starting index).
+ */
+
+/**
+ *
+ * @param {String} s
+ * @param {Number} low
+ * @param {Number} high
+ * @returns
+ * Boolean whether string is palindrome or not
+ */
+const palindrome = (s, low, high) =>  {
+  let flag = true;
+  const subString = s.substring(low, high )
+  let localLow = 0
+  let localHigh = subString.length - 1
+  while(localLow < localHigh) {
+    if(subString[localLow] === subString[localHigh]) {
+      localLow++;
+      localHigh--;
+      continue
+    } else flag = false;
+    break;
+  }
+  return { flag, subString}
+}
+
+const longestPalindrome = (S) => {
+  let palindromeIterator = 2
+  const stringLength = S.length
+  let maxPalindrome = S[0]
+
+  // Sliding Window
+  while(palindromeIterator < stringLength) {
+    for (let k = 0; k <= stringLength - palindromeIterator + 1; k++) {
+      const { flag: isPalindrome, subString } = palindrome(S, k, k + palindromeIterator + 1)
+      if( isPalindrome ) {
+        maxPalindrome = subString
+        break
+      }
+    }
+    palindromeIterator++
+  }
+
+  return maxPalindrome
+}
+
+console.log('longest palindrome is => ', longestPalindrome('rfkqyuqfjkxy') )
