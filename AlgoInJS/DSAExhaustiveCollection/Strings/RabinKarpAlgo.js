2ae114c4806d57451d9c9534faf13ab9766c1956
shubham43MP <shubhamdixit19950321@gmail.com>
2021-08-09 18:18:43 +0530

    [DSA] Added Rabin Karp Algorithm

--- AlgoInJS/DSAExhaustiveCollection/Strings/RabinKarpAlgo.js
+++ AlgoInJS/DSAExhaustiveCollection/Strings/RabinKarpAlgo.js
@@ -6,6 +6,17 @@
  */
 
  class Solution {
+   constructor() {
+     this.primeNumberForHash = 5381
+     this.d = 256
+   }
+
+  /**
+   * @param {String} input
+   * @param {String} pattern
+   * T(n) = O(N *  length of pattern)
+   * S(n) = O(1)
+   */
   naiveStringMatching(input, pattern) {
     const patternLength = pattern.length
     for (let k = 0; k < input.length; k++) {
@@ -13,6 +24,37 @@
       if(input.substring(k, end + 1) === pattern) {
         console.log('Desired index => ', k)
       }
+    }
+  }
+
+  rabinKarpAlgoStringMatch(txt, pat) {
+    // This is also called rolling hash Algorithm
+    let M = pat
+    let N = txt.length
+    let patternHash = 0
+    let txtHash = 0
+    let h = 1
+
+    for(let i = 0; i < M - 1; i++)
+        h = (h * this.d) % this.primeNumberForHash
+    for (let k = 0; k < M - 1; k++) {
+      patternHash = (this.d * patternHash + pat[k].charCodeAt()) % this.primeNumberForHash
+      txtHash = (this.d * txtHash + txt[k].charCodeAt()) % this.primeNumberForHash
+    }
+
+    for (let i = 0; i <= N - M; i++) {
+      if(patternHash === txtHash) {
+        for(let j = 0; j < M; j++) {
+            if (txt[i+j] != pat[j])
+                break
+        }
+        console.log('Pattern found at ', i)
+      }
+      if(i < N-M) {
+        txtHash = (this.d * (txtHash - txt[i].charCodeAt() * h) + txt[i + M].charCodeAt()) % this.primeNumberForHash
+        if( txtHash < 0 ) txtHash = txtHash + this.primeNumberForHash
+      }
+
     }
   }
 }
@@ -31,3 +73,4 @@ const solution = new Solution()
 const input = "AABAACAADAABAABA"
 const pattern = "AABA" // 10
 solution.naiveStringMatching(input, pattern)
+solution.rabinKarpAlgoStringMatch(input, pattern)
