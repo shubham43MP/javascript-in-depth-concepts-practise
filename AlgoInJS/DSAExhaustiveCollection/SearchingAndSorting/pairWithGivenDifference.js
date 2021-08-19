0f7b8711c49a8246e079383b0e38ee9f8b82d3a0
shubham43MP <shubhamdixit19950321@gmail.com>
2021-08-19 19:06:34 +0530

    [DSA] Added solution for pair search with given attributes

---
+++ AlgoInJS/DSAExhaustiveCollection/SearchingAndSorting/pairWithGivenDifference.js
@@ -0,0 +1,57 @@
+/**
+ * Given an array Arr[] of size L and a number N, you need to write a program to find if there exists
+ * a pair of elements in the array whose difference is N.
+ */
+
+
+class Solution {
+
+  /**
+   *
+   * @param {Array} nums
+   * @param {Number} start
+   * @param {Number} end
+   * @param {Number} target
+   * @returns
+   */
+  pointToPointBinarySearch(nums, start, end, target) {
+    while(start <= end) {
+      const middle = Math.floor( start + (end - start)/2 )
+      if( nums[middle] === target) return middle
+      else if(nums[middle] > target) end = middle - 1
+      else if(nums[middle] < target) start = middle + 1
+    }
+    return -1
+  }
+
+  /**
+   *
+   * @param {Array} arr
+   * @param {Number} L
+   * @param {Number} N
+   * @returns
+   * Whether a pair exists in given arr whose difference equals to N
+   */
+  differencePair(arr, L, N) {
+    // Sort the array in L*log(L)
+    arr.sort((a,b) => a-b)
+    for (let i = 0; i < arr.length; i++) {
+      const element = arr[i];
+      const operativeKey = element + N
+      const searchOK = this.pointToPointBinarySearch(arr, i + 1, L - 1, operativeKey)
+      if(searchOK !== -1 ) return 1
+    }
+    return -1
+  }
+}
+
+
+const solution = new Solution()
+// const arr = [5, 20, 3, 2, 5, 80]
+// const L = arr.length
+// const N = 78 // True
+
+const arr = [90, 70, 20, 80, 50]
+const L = arr.length
+const N = 45 // false
+console.log('Such difference exists ==>', solution.differencePair(arr, L, N))
