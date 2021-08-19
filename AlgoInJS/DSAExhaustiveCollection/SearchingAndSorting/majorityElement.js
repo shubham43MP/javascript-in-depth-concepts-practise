c427ffd2609666a3922fc5dbc23b129561e46781
shubham43MP <shubhamdixit19950321@gmail.com>
2021-08-19 18:02:43 +0530

    [DSA] Updated docs

--- AlgoInJS/DSAExhaustiveCollection/SearchingAndSorting/majorityElement.js
+++ AlgoInJS/DSAExhaustiveCollection/SearchingAndSorting/majorityElement.js
@@ -1,4 +1,5 @@
 /**
+ * [GFG Accepted]
  * Given an array A of N elements.
  * Find the majority element in the array.
  * A majority element in an array A of size N is an element that appears more than N/2 times in the array.
