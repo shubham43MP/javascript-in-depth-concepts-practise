645395e7391ab5657dc111df72e3724c6e7707ac
shubham43MP <shubhamdixit19950321@gmail.com>
2023-03-05 19:06:44 +0530

    Lecture 9 finalkised

--- js-oops/concepts.md
+++ js-oops/concepts.md
@@ -7,3 +7,14 @@
 ## Inheritance
 
 ## Polymorphism
+
+
+## Factory Function
+- A function that returns a configurable object to create is a factory function. `Refer oops-p1.js`
+- Those who prefer functional sorts of programming away from classical OOPS can prefer Factory functions.
+- Javascript is not a classical OOPS based language
+
+## Constructor Function
+- A function that  utilising the new keyword  as well as the this keyword for purposes of instance or so is a Concturctor function
+- More close to C#, Java, C++ approach.
+- Without the use of new keyword here, the this would point to global object
--- js-oops/oops-p1.js
+++ js-oops/oops-p1.js
@@ -1,1 +1,42 @@
-console.log('Oops Phase 1 is up')
+const circle = {
+  radius: 1,
+  location: {
+    x: 1,
+    y: 1
+  },
+  draw: function() {
+    console.log("draw")
+  }
+}
+
+circle.draw()
+
+/*
+Factories: Used to create multiple objects as duplicating an object would be problem, we have to copy paste uit a lot
+
+
+*/
+// This is a factory function
+function createCircle(radius) {
+  const circle = {
+    radius,
+    draw: function() {
+      console.log("draw from factory")
+    }
+  }
+  return circle
+}
+
+createCircle(5).draw()
+
+// Constructor Function
+// the new operator will first set the this scope to the object and then retyrnj the object itself
+function Circle(radius) {
+
+  this.radius = radius
+  this.draw = function() {
+    console.log("Draw from Constructor function")
+  }
+}
+
+const another = new Circle(1)
