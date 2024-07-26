10c9f942530bf522710595c74eed308cfc7e2b06
shubham43MP <shubhamdixit19950321@gmail.com>
2024-07-26 18:54:19 +0530

    updated minor doc

--- objects/map.js
+++ objects/map.js
@@ -1,6 +1,8 @@
 /**
  * Objects and Map have similar use cases but still they are different. Lets explore them such.
- * Key field: in Object, it follows the rule of normal dictionary. The keys MUST be simple types — either integer or string or symbols. Nothing more. But in Map it can be any data type (an object, an array, etc…). (try using another object as Object’s property key — i dare you :))
+ * Key field: in Object, it follows the rule of normal dictionary. The keys MUST be simple types —
+ * either integer or string or symbols. Nothing more. But in Map it can be any data type (an object, an array, etc…).
+ * (try using another object as Object’s property key — i dare you :))
 Element order: in Map, original order of elements (pairs) is preserved, while in Object, it isn’t.
 Inheritance: Map is an instance of Object (surprise surprise!), but Object is definitely not an instance of Map.
  */
